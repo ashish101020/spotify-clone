9e147c30cde7cd842fb10b57c31374e30148f742
ashish <guptashish101020@gmail.com>
2025-06-27 19:30:55 +0530

    Navbar created in DisplayHome

--- src/Components/Player.jsx
+++ src/Components/Player.jsx
@@ -12,11 +12,7 @@ const Player = () => {
       </div>
       <div className="flex flex-col items-center fap-1 m-auto">
         <div className="flex gap-4">
-          <img
-            className="w-4 cursor-pointer"
-            src={assets.shuffle_icon}
-            alt=""
-          />
+          <img className="w-4 cursor-pointer" src={assets.shuffle_icon} alt="" />
           <img className="w-4 cursor-pointer" src={assets.prev_icon} alt="" />
           <img className="w-4 cursor-pointer" src={assets.play_icon} alt="" />
           <img className="w-4 cursor-pointer" src={assets.next_icon} alt="" />
@@ -28,13 +24,24 @@ const Player = () => {
           <div className="relative max-w-[500px] w-[60vw] h-1 bg-gray-300 rounded-full cursor-pointer">
             <div
               className="absolute top-0 left-0 h-full bg-green-800 rounded-full"
-              style={{ width: "30%" }}
+              style={{ width: "50%" }}
             ></div>
           </div>
 
           <p>3:20</p>
         </div>
       </div>
+      <div className="hidden lg:flex items-center gap-2 opacity-75">
+        <img className="w-4" src={assets.play_icon} alt="" />
+        <img className="w-4" src={assets.mic_icon} alt="" />
+        <img className="w-4" src={assets.queue_icon} alt="" />
+        <img className="w-4" src={assets.speaker_icon} alt="" />
+        <img className="w-4" src={assets.volume_icon} alt="" />
+        <div className="w-20 bg-slate-50 h-1 rounded"></div>
+        <img className="w-4" src={assets.mini_player_icon} alt="" />
+        <img className="w-4" src={assets.zoom_icon} alt="" />
+      </div>
+
     </div>
   );
 };
