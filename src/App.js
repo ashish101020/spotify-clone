a87249bd5d8fcade2436ca304a144f3fd7add93b
ashish <guptashish101020@gmail.com>
2025-06-27 18:31:55 +0530

    assets directory change

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import Player from "./Components/Player";
 import Sidebar from "./Components/Sidebar";
 
 export default function App() {
@@ -6,6 +7,7 @@ export default function App() {
       <div className="h-[90%] flex">
         <Sidebar/>
       </div>
+      <Player/>
     </div>
   )
 }
---
+++ src/Components/Player.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { assets, songsData } from "../Assets/assets";
+const Player = () => {
+  return (
+    <div className="h-[10%] bg-black flex justify-between items-center text-white -x-4">
+      <div className="hidden lg:flex items-center gap-4 pl-5">
+        <img className="w-12 " src={songsData[0].image} alt="" />
+        <div className="">
+          <p>{songsData[0].name}</p>
+          <p>{songsData[0].desc.slice(0, 12)}</p>
+        </div>
+      </div>
+      <div className="flex flex-col items-center fap-1 m-auto">
+        <div className="flex gap-4">
+          <img
+            className="w-4 cursor-pointer"
+            src={assets.shuffle_icon}
+            alt=""
+          />
+          <img className="w-4 cursor-pointer" src={assets.prev_icon} alt="" />
+          <img className="w-4 cursor-pointer" src={assets.play_icon} alt="" />
+          <img className="w-4 cursor-pointer" src={assets.next_icon} alt="" />
+          <img className="w-4 cursor-pointer" src={assets.loop_icon} alt="" />
+        </div>
+        <div className="flex items-center gap-5">
+          <p>1:06</p>
+
+          <div className="relative max-w-[500px] w-[60vw] h-1 bg-gray-300 rounded-full cursor-pointer">
+            <div
+              className="absolute top-0 left-0 h-full bg-green-800 rounded-full"
+              style={{ width: "30%" }}
+            ></div>
+          </div>
+
+          <p>3:20</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Player;
